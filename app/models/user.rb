51363125b360b6bd13caac4cab1f0a22189d2b07
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-15 18:28:40 +0900

    deviseの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         authentication_keys: [:username]
+         :recoverable, :rememberable, :trackable, :validatable
 
   # usernameを必須・一意とする
   validates_uniqueness_of :username, :iidxid
