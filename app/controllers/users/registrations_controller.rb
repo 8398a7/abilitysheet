3f54590dc942c95c12b4aa6c398b6fa6e4c10a95
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-17 01:14:16 +0900

    noticeモデルを作成，登録人数を100人まで

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,15 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     after_action :score_create, only: [:create]
 
+    def create
+      count = User.all.count
+      if 100 <= count
+        flash[:alert] = '登録人数が上限に達しました，追加をお待ちください'
+        redirect_to root_path
+      end
+      super
+    end
+
     private
 
     def score_create
---
+++ db/migrate/20141116153743_create_notices.rb
@@ -0,0 +1,11 @@
+class CreateNotices < ActiveRecord::Migration
+  def change
+    create_table :notices do |t|
+      t.string :body
+      t.integer :state
+      t.boolean :active
+
+      t.date :created_at
+    end
+  end
+end
