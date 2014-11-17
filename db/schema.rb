32a2208cfbec6d03d4ff401459d01179227d76b6
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-18 03:40:36 +0900

    messageモデルにインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116172112) do
+ActiveRecord::Schema.define(version: 20141117183906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20141116172112) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "notices", force: true do |t|
     t.string  "body"
