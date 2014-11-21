5d94dc0358cef6748728ffc54fbfe114f9f9f961
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-21 16:20:40 +0900

    capistranoのログレベルをデバッグに

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :bundle_path, -> { shared_path.join('vendor/bundle') }
 set :format, :pretty
 
 # Default value for :log_level is :debug
-set :log_level, :info
+set :log_level, :debug
 
 # Default value for :pty is false
 # set :pty, true
