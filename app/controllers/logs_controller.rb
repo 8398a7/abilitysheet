b5bb2e34f0df6309ad4668b71c3b3141ca4c8fa1
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-21 19:13:37 +0900

    更新履歴のツイートボタン追加，各ログから前後のログへ移動を実装

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -22,6 +22,8 @@ class LogsController < ApplicationController
   def show
     user_id = User.find_by(iidxid: params[:iidxid]).id
     @logs = Log.where(user_id: user_id, created_at: params[:date]).preload(:sheet)
+    list = User.find_by(iidxid: params[:iidxid]).logs.pluck(:created_at).uniq
+    @prev_update, @next_update = prev_next(user_id, params[:date])
     @color = Score.list_color
   end
 
@@ -96,5 +98,17 @@ class LogsController < ApplicationController
       break if s == l + 1.months
     end
     array
+  end
+
+  def prev_next(user_id, created_at)
+    logs = User.find_by(id: user_id).logs.order(:created_at).pluck(:created_at).uniq
+    prev_u, next_u = nil, nil
+    (0..logs.count-1).each do |cnt|
+      if logs[cnt].strftime == created_at
+        prev_u = logs[cnt-1] if 0 <= cnt-1
+        next_u = logs[cnt+1] if cnt+1 <= logs.count-1
+      end
+    end
+    return prev_u, next_u
   end
 end
