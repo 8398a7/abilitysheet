be3e3afc2141d753ad050d8e549269b0a0cdc691
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2014-11-24 04:51:01 +0900

    sidekiqを利用しない

--- app/workers/maneger_worker.rb
+++ app/workers/maneger_worker.rb
@@ -3,6 +3,7 @@ class ManegerWorker
   sidekiq_options queue: :maneger
   def perform(id)
     current_user = User.find_by(id: id)
+    puts %(#{ Time.now} #{ current_user.djname }[#{ current_user.iidxid }] => maneger scrape start)
     scrape = Scrape::Maneger.new(current_user)
     scrape.sync
     puts %(#{ Time.now} #{ current_user.djname }[#{ current_user.iidxid }] => maneger scrape done)
