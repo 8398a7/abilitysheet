7c1e0cacc72236d7a404c5d333bcdb46fc21f6c1
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2015-01-12 22:17:16 +0900

    testディレクトリの削除

--- test/mailers/previews/notice_mail_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notice_mail
-class NoticeMailPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/notice_mail/sendMail
-  def sendMail
-    NoticeMail.sendMail
-  end
-
-end
