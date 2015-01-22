44d5dbee58794f78787f2fe30ef78bf5a4a9b2c6
husq <s14g469@stmail.eng.kagawa-u.ac.jp>
2015-01-23 00:49:04 +0900

    #10 未クリア表示部分の作成

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -19,8 +19,23 @@ class SheetsController < ApplicationController
     @sheets = @sheets.where(version: params[:version]) if params[:version] && params[:version] != '0'
   end
 
+  def version_stat
+    @version_stat = []
+    versions = (5..22).map(&:to_i)
+    scores = User.find_by(iidxid: params[:iidxid]).scores
+    state = 5
+    sum = 0
+    versions.each do |version|
+      sheet_ids = @sheets.where(version: version).pluck(:id)
+      @version_stat.push(scores.where(sheet_id: sheet_ids, state: state..7).count)
+    end
+    @version_stat.each { |v| sum += v }
+    @version_stat.push(sum)
+  end
+
   def set_sheet
     @sheets = Sheet.active
+    version_stat
     version_check
     @state_examples = {}
     7.downto(0) { |j| @state_examples[Score.list_name[j]] = Score.list_color[j] }
