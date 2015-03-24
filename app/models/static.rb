c4e95faef74a5bddefb1f743589438439b5994e6
8398a7 <s14g469@stmail.eng.kagawa-u.ac.jp>
2015-03-24 21:07:45 +0900

    close #21 APIを叩いて地力値の更新

--- app/models/static.rb
+++ app/models/static.rb
@@ -1,3 +1,10 @@
 class Static < ActiveRecord::Base
   belongs_to :sheet
+
+  def self.async(data)
+    data.each do |d|
+      static = find_by(sheet_id: d['sheet_id'])
+      static.update(fc: d['fc'], exh: d['exh'], h: d['h'], c: d['c'], e: d['e'])
+    end
+  end
 end
