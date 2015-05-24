a22773a29815645fac0490ac2f2c55608d548945
8398a7 <s14g469@stmail.eng.kagawa-u.ac.jp>
2015-05-24 13:31:07 +0900

    root_pathをデータが空の時に表示できない不具合を修正

--- spec/controllers/sheets_controller_spec.rb
+++ spec/controllers/sheets_controller_spec.rb
@@ -2,20 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SheetsController, type: :request do
   let(:user) { FactoryGirl.create(:user) }
-  describe 'ハード参考表ページ' do
-    context 'ログイン時' do
-      before do
-        login_as(user, scope: :user, run_callbacks: false)
-        visit hard_sheets_path(user.iidxid)
-      end
-      it 'ハード参考表の文字が存在する' do
-        expect(page).to have_content('ハード参考表')
-      end
-      it 'クリア参考表のリンクが存在する' do
-        expect(page).to have_link('CLEAR', clear_sheets_path(user.iidxid))
-      end
+  context 'ログイン時' do
+    before { login_as(user, scope: :user, run_callbacks: false) }
+
+    it 'ログインしている' do
+      visit root_path
+      expect(page).to have_link('ログアウト')
     end
-    context '非ログイン時' do
+
+    describe 'ハード参考表ページ' do
       before { visit hard_sheets_path(user.iidxid) }
       it 'ハード参考表の文字が存在する' do
         expect(page).to have_content('ハード参考表')
@@ -24,22 +19,8 @@ RSpec.describe SheetsController, type: :request do
         expect(page).to have_link('CLEAR', clear_sheets_path(user.iidxid))
       end
     end
-  end
 
-  describe 'ノマゲ参考表ページ' do
-    context 'ログイン時' do
-      before do
-        login_as(user, scope: :user, run_callbacks: false)
-        visit clear_sheets_path(user.iidxid)
-      end
-      it 'ノマゲ参考表の文字が存在する' do
-        expect(page).to have_content('ノマゲ参考表')
-      end
-      it 'ハード参考表のリンクが存在する' do
-        expect(page).to have_link('HARD', hard_sheets_path(user.iidxid))
-      end
-    end
-    context '非ログイン時' do
+    describe 'ノマゲ参考表ページ' do
       before { visit clear_sheets_path(user.iidxid) }
       it 'ノマゲ参考表の文字が存在する' do
         expect(page).to have_content('ノマゲ参考表')
@@ -48,20 +29,42 @@ RSpec.describe SheetsController, type: :request do
         expect(page).to have_link('HARD', hard_sheets_path(user.iidxid))
       end
     end
-  end
 
-  describe '地力値参考表ページ' do
-    context 'ログイン時' do
-      before do
-        login_as(user, scope: :user, run_callbacks: false)
-        visit power_sheets_path(user.iidxid)
-      end
+    describe '地力値参考表ページ' do
+      before { visit power_sheets_path(user.iidxid) }
       it 'iidx.meの文字が存在する' do
-        save_and_open_page
         expect(page).to have_content('iidx.me')
       end
     end
-    context '非ログイン時' do
+  end
+
+  context '非ログイン時' do
+    it 'ログインしていない' do
+      visit root_path
+      expect(page).to have_link('ログイン')
+    end
+
+    describe 'ハード参考表ページ' do
+      before { visit hard_sheets_path(user.iidxid) }
+      it 'ハード参考表の文字が存在する' do
+        expect(page).to have_content('ハード参考表')
+      end
+      it 'クリア参考表のリンクが存在する' do
+        expect(page).to have_link('CLEAR', clear_sheets_path(user.iidxid))
+      end
+    end
+
+    describe 'ノマゲ参考表ページ' do
+      before { visit clear_sheets_path(user.iidxid) }
+      it 'ノマゲ参考表の文字が存在する' do
+        expect(page).to have_content('ノマゲ参考表')
+      end
+      it 'ハード参考表のリンクが存在する' do
+        expect(page).to have_link('HARD', hard_sheets_path(user.iidxid))
+      end
+    end
+
+    describe '地力値参考表ページ' do
       before { visit power_sheets_path(user.iidxid) }
       it 'iidx.meの文字が存在する' do
         expect(page).to have_content('iidx.me')
@@ -69,3 +72,4 @@ RSpec.describe SheetsController, type: :request do
     end
   end
 end
+
