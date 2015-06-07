b9626fc1cccd59d26ed8450227097bc24d313c5a
8398a7 <8398a7@gmail.com>
2015-06-08 06:16:50 +0900

    close #55

--- spec/factories/sheets.rb
+++ spec/factories/sheets.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :sheet do
     title "MyString"
-ability 1
-h_ability 1
-version 1
-active false
-textage "MyString"
+    ability 1
+    h_ability 1
+    version 1
+    active false
+    textage "MyString"
   end
-
 end
