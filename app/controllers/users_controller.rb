d00047294f1e79983f6c7e7a7fc86650eedc4cd0
839 <8398a7@gmail.com>
2015-06-21 13:44:40 +0900

    refs #86 ユーザ一覧をajaxで取るように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
   def index
     @cnt = User.select(:id).count
-    if params[:query] && params[:query].present?
-      @users = User.search_djname(params[:query].upcase)
-    else
-      user_ids = Score.order(updated_at: :desc).pluck(:user_id).uniq
-      user_ids.slice!(200, user_ids.count - 1)
-      @users = User.where(id: user_ids)
+    @users = User.all
+  end
+
+  def call_back
+    json = {}
+    color = Static::COLOR
+    users = User.where(id: JSON.parse(params[:id]))
+    users.each do |user|
+      score = user.scores.order(updated_at: :desc).find_by(state: 0..6)
+      unless score
+        json[user.id] = { title: '', stateColor: '', updatedAt: '' }
+        next
+      end
+      score = score.decorate
+      json[user.id] = {
+        title: score.title,
+        stateColor: color[score.state],
+        updatedAt: "<a href=\"#{show_log_path(user.iidxid, score.updated_at.to_date.strftime)}\">#{score.updated_at}</a>"
+      }
     end
-    @color = Static::COLOR
+    render json: json
   end
 end
