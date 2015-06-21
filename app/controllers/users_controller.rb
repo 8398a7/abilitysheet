2b3c7d78fc6e812557b32dab4009888ed78f7fa5
839 <8398a7@gmail.com>
2015-06-21 14:05:37 +0900

    close #86 ユーザすべてを取得する処理をリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
+  before_action :load_users
+
   def index
     @cnt = User.select(:id).count
-    @users = User.all
   end
 
   def call_back
     json = {}
     color = Static::COLOR
-    users = User.where(id: JSON.parse(params[:id]))
-    users.each do |user|
-      score = user.scores.order(updated_at: :desc).find_by(state: 0..6)
+    @users.each do |user|
+      score = user.scores.last_updated
       unless score
         json[user.id] = { title: '', stateColor: '', updatedAt: '' }
         next
@@ -18,9 +18,15 @@ class UsersController < ApplicationController
       json[user.id] = {
         title: score.title,
         stateColor: color[score.state],
-        updatedAt: "<a href=\"#{show_log_path(user.iidxid, score.updated_at.to_date.strftime)}\">#{score.updated_at}</a>"
+        updatedAt: "<a href=\"#{show_log_path(user.iidxid, score.updated_at)}\">#{score.updated_at}</a>"
       }
     end
     render json: json
+  end
+
+  private
+
+  def load_users
+    @users = User.all
   end
 end
