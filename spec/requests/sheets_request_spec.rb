12adc30d0b0bbd070242d1754f31c0603e695016
839 <8398a7@gmail.com>
2015-06-24 10:40:02 +0900

    close #88 楽曲状態更新に関するViewのテストを記述

--- spec/requests/sheets_request_spec.rb
+++ spec/requests/sheets_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SheetsController, type: :request do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user, id: 1) }
   context 'ログイン時' do
     before { login_as(user, scope: :user, run_callbacks: false) }
 
@@ -69,6 +69,32 @@ RSpec.describe SheetsController, type: :request do
       it 'iidx.meの文字が存在する' do
         expect(page).to have_content('iidx.me')
       end
+    end
+  end
+  context '楽曲更新時' do
+    before do
+      page.driver.allow_url('platform.twitter.com')
+      FactoryGirl.create(:sheet, id: 1, active: true)
+      FactoryGirl.create(:score, id: 1, user_id: 1, sheet_id: 1, state: 6)
+      login_as(user, scope: :user, run_callbacks: false)
+      visit clear_sheets_path(user.iidxid)
+    end
+
+    it 'モーダルが降りてくる', js: true do
+      click_on 'MyString'
+      expect(page).to have_content('クリア情報更新')
+    end
+
+    it '楽曲が更新でき，ログが作られている', js: true do
+      expect(Score.find_by(id: 1).state).to eq 6
+      expect(Log.exists?(user_id: 1, sheet_id: 1, pre_state: 6, new_state: 3)).to eq false
+      click_on 'MyString'
+      sleep(1)
+      select 'CLEAR', from: 'score_state'
+      click_on '更新'
+      visit clear_sheets_path(user.iidxid)
+      expect(Score.find_by(id: 1).state).to eq 3
+      expect(Log.exists?(user_id: 1, sheet_id: 1, pre_state: 6, new_state: 3)).to eq true
     end
   end
 end
