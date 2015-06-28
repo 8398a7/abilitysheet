ea61fdafdfd82e3e825ae3325f1062ac6866776b
839 <8398a7@gmail.com>
2015-06-29 02:43:01 +0900

    書式の修正

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe MessagesController, type: :controller do
   include Devise::TestHelpers
 
-  context '#new' do
+  describe 'GET #new' do
     it '新しいページが正しく開ける' do
       get :new
       expect(assigns(:message)).to be_a_new(Message)
@@ -11,7 +11,7 @@ RSpec.describe MessagesController, type: :controller do
     end
   end
 
-  context '#create' do
+  describe 'POST #create' do
     it '新しくメッセージを作る' do
       expect do
         post :create, message: { body: 'test' }
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,22 +8,22 @@ RSpec.describe UsersController, type: :controller do
       FactoryGirl.create(:user, iidxid: format('0000-%04d', i), username: format('test%d', i))
     end
   end
-  context '#index' do
+  describe 'GET #index' do
     before { get :index }
     it 'アクセスできる' do
-      expect(response.code).to eq '200'
+      expect(response).to have_http_status(:success)
     end
     it 'プレイヤー数の取得が正しい' do
       expect(assigns(:cnt)).to eq 2
     end
   end
 
-  context '#call_back' do
+  describe 'POST #call_back' do
     before do
       post :call_back, id: '[1, 2, 3]'
     end
     it 'アクセスできる' do
-      expect(response.code).to eq '200'
+      expect(response).to have_http_status(:success)
     end
     it 'jsonが返ってくる' do
       ret = '{"1":{"title":"","stateColor":"","updatedAt":""},"2":{"title":"","stateColor":"","updatedAt":""}}'
