1d11ef2a652f6e2b81d096353331b84af4da5575
839 <8398a7@gmail.com>
2015-06-29 02:12:21 +0900

    rspecの修正

--- spec/requests/api_developer_spec.rb
+++ spec/requests/api_developer_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe V22::Developer, type: :request do
   include_context 'api'
-  context 'POST /api/v22/developer/sheets' do
+  describe 'POST /api/v22/developer/sheets' do
     before { FactoryGirl.create(:sheet, id: 1) }
     let(:url) { '/api/v22/developer/sheets' }
     let(:method) { 'post' }
     context 'admin user以外' do
       let(:user) { FactoryGirl.create(:user, admin: false) }
-      describe 'パスワードが正しい' do
+      context 'パスワードが正しい' do
         let(:parameters) do
           { user: user.username, password: user.password }
         end
         it_behaves_like '403 Forbidden'
       end
-      describe 'パスワードが誤り' do
+      context 'パスワードが誤り' do
         let(:parameters) do
           { user: user.username, password: 'hogefoobar' }
         end
@@ -53,7 +53,7 @@ RSpec.describe V22::Developer, type: :request do
       end
     end
   end
-  context 'POST /api/v22/developer/users' do
+  describe 'POST /api/v22/developer/users' do
     before { FactoryGirl.create(:user) }
     let(:url) { '/api/v22/developer/users' }
     let(:method) { 'post' }
