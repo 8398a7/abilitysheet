c18c93373875934b475edeca6fe90b578885e161
839 <8398a7@gmail.com>
2015-06-30 08:26:23 +0900

    sheetsの変更に追従

--- spec/requests/sheets_request_spec.rb
+++ spec/requests/sheets_request_spec.rb
@@ -11,27 +11,27 @@ RSpec.describe SheetsController, type: :request do
     end
 
     describe 'ハード参考表ページ' do
-      before { visit hard_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'hard') }
       it 'ハード参考表の文字が存在する' do
         expect(page).to have_content('ハード参考表')
       end
       it 'クリア参考表のリンクが存在する' do
-        expect(page).to have_link('CLEAR', clear_sheets_path(user.iidxid))
+        expect(page).to have_link('CLEAR', sheet_path(iidxid: user.iidxid, type: 'clear'))
       end
     end
 
     describe 'ノマゲ参考表ページ' do
-      before { visit clear_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'clear') }
       it 'ノマゲ参考表の文字が存在する' do
         expect(page).to have_content('ノマゲ参考表')
       end
       it 'ハード参考表のリンクが存在する' do
-        expect(page).to have_link('HARD', hard_sheets_path(user.iidxid))
+        expect(page).to have_link('HARD', sheet_path(iidxid: user.iidxid, type: 'hard'))
       end
     end
 
     describe '地力値参考表ページ' do
-      before { visit power_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'power') }
       it 'iidx.meの文字が存在する' do
         expect(page).to have_content('iidx.me')
       end
@@ -45,27 +45,27 @@ RSpec.describe SheetsController, type: :request do
     end
 
     describe 'ハード参考表ページ' do
-      before { visit hard_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'hard') }
       it 'ハード参考表の文字が存在する' do
         expect(page).to have_content('ハード参考表')
       end
       it 'クリア参考表のリンクが存在する' do
-        expect(page).to have_link('CLEAR', clear_sheets_path(user.iidxid))
+        expect(page).to have_link('CLEAR', sheet_path(iidxid: user.iidxid, type: 'clear'))
       end
     end
 
     describe 'ノマゲ参考表ページ' do
-      before { visit clear_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'clear') }
       it 'ノマゲ参考表の文字が存在する' do
         expect(page).to have_content('ノマゲ参考表')
       end
       it 'ハード参考表のリンクが存在する' do
-        expect(page).to have_link('HARD', hard_sheets_path(user.iidxid))
+        expect(page).to have_link('HARD', sheet_path(iidxid: user.iidxid, type: 'hard'))
       end
     end
 
     describe '地力値参考表ページ' do
-      before { visit power_sheets_path(user.iidxid) }
+      before { visit sheet_path(iidxid: user.iidxid, type: 'power') }
       it 'iidx.meの文字が存在する' do
         expect(page).to have_content('iidx.me')
       end
@@ -77,7 +77,7 @@ RSpec.describe SheetsController, type: :request do
       create(:sheet, id: 1, active: true)
       create(:score, id: 1, user_id: 1, sheet_id: 1, state: 6)
       login_as(user, scope: :user, run_callbacks: false)
-      visit clear_sheets_path(user.iidxid)
+      visit sheet_path(iidxid: user.iidxid, type: 'clear')
     end
 
     it 'モーダルが降りてくる', js: true do
@@ -92,7 +92,7 @@ RSpec.describe SheetsController, type: :request do
       sleep(1)
       select 'CLEAR', from: 'score_state'
       click_on '更新'
-      visit clear_sheets_path(user.iidxid)
+      visit sheet_path(iidxid: user.iidxid, type: 'clear')
       expect(Score.find_by(id: 1).state).to eq 3
       expect(Log.exists?(user_id: 1, sheet_id: 1, pre_state: 6, new_state: 3)).to eq true
     end
