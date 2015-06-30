de294f8afa2845fb58dde82ce392d22c9ec3158d
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-30 22:55:38 +0900

    feature: user decorators test based on codeclimate

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,41 @@
 require 'spec_helper'
 
 describe UserDecorator do
+  let(:instance) do
+    described_class.new(user)
+  end
+
+  let(:user) do
+    create(:user)
+  end
+
+  describe '#pref' do
+    it 'returns pref of its pref' do
+      expect(instance.pref).to eq('海外')
+    end
+  end
+
+  describe '#grade' do
+    it 'returns dan grade of its grade' do
+      expect(instance.grade).to eq('皆伝')
+    end
+  end
+
+  describe '#belongs' do
+    it 'returns belongs of its pref' do
+      expect(instance.pref).to eq('海外')
+    end
+  end
+
+  describe '#dan' do
+    it 'returns dan of its grade' do
+      expect(instance.dan).to eq('皆伝')
+    end
+  end
+
+  describe '#dan_color' do
+    it 'returns dan color of its grade' do
+      expect(instance.dan_color).to eq('#ffd900')
+    end
+  end
 end
