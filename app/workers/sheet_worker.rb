b2c1f6d44d50c6817022d1de1a70537a11e7d534
839 <8398a7@gmail.com>
2015-07-02 12:57:12 +0900

    楽曲が既に存在している時としていない時でメッセージを分ける

--- app/workers/sheet_worker.rb
+++ app/workers/sheet_worker.rb
@@ -5,15 +5,19 @@ class SheetWorker
 
   def perform(id)
     puts %(#{Time.now} sheet_id: #{id} => create score and ability start)
-    unless Sheet.exists?(id: id)
-      version = Abilitysheet::Application.config.iidx_version
-      User.all.each { |u| Score.create(user_id: u.id, sheet_id: id, version: version) }
-      d = 99.99
-      Ability.create(
-        sheet_id: id,
-        fc: d, exh: d, h: d, c: d, e: d, aaa: d
-      )
+
+    if Sheet.exists?(id: id)
+      puts %(#{Time.now} sheet_id: #{id} => !!!!exist score and ability!!!!!)
+      return
     end
+
+    version = Abilitysheet::Application.config.iidx_version
+    User.all.each { |u| Score.create(user_id: u.id, sheet_id: id, version: version) }
+    d = 99.99
+    Ability.create(
+      sheet_id: id,
+      fc: d, exh: d, h: d, c: d, e: d, aaa: d
+    )
     puts %(#{Time.now} sheet_id: #{id} => create score and ability end)
   end
 end
