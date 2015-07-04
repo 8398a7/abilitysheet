be930fb095c74bc65810a959eb2cf5e31d24cf6d
839 <8398a7@gmail.com>
2015-07-04 17:53:29 +0900

    errbitのAPIKEYをenvに設定

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = 'c5d8da5efd55c8bfc1daf84054496ea0'
+  config.api_key = ENV['ERRBIT_API_KEY']
   config.host    = 'iidx12.herokuapp.com'
   config.port    = 443
   config.secure  = config.port == 443
