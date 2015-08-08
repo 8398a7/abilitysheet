3460fbb05209f323d6852ab806792b8c95471a88
839 <8398a7@gmail.com>
2015-08-08 18:37:35 +0900

    DJNAME検索時とそれ以外で表示するテンプレートを分ける

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def index
     @cnt = User.select(:id).count
-    uri    = URI.parse('http://localhost:8080/api/v1/users/recent200')
-    res    = Net::HTTP.get(uri)
-    @users = JSON.parse(res)
+    if params[:query] && params[:query].present?
+      @users = User.search_djname(params[:query].upcase)
+      @scores_map = User.users_list(:rivals, @users)
+    else
+      uri    = URI.parse('http://localhost:8080/api/v1/users/recent200')
+      res    = Net::HTTP.get(uri)
+      @users = JSON.parse(res)
+    end
   end
 end
