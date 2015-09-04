7ae323eee1227ff8688983b6bb1037a2e0c5d3e8
839 <8398a7@gmail.com>
2015-09-04 22:12:00 +0900

    refactoring setting for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
-require 'factory_girl_rails'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
-require 'database_cleaner'
-require 'warden'
-require 'devise'
 require 'sidekiq/testing'
 
 RSpec.configure do |config|
@@ -15,30 +11,14 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  config.include Warden::Test::Helpers
-  config.include FactoryGirl::Syntax::Methods
+
   config.include Capybara::DSL
 
-  config.order = 'random'
-
-  config.before(:all) do
-    Capybara.default_selector = :css
-    Capybara.javascript_driver = :poltergeist
+  Capybara.default_selector = :css
+  Capybara.javascript_driver = :poltergeist
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, inspector: true)
   end
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-end
-
-Sidekiq::Testing.inline!
-
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, inspector: true)
+  Sidekiq::Testing.inline!
 end
