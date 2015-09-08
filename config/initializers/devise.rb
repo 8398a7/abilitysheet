b5ea66bbd604fda03cf73362bbdb81d95e711a23
839 <8398a7@gmail.com>
2015-09-09 05:13:04 +0900

    refs #211 メールの送信元情報を修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,12 +6,11 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   config.secret_key = ENV['SECRET_KEY_BASE']
 
-
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'abilitysheet@iidx12.tk'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
