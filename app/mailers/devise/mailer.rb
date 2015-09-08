769173ad30269938562966e6e7d77e6791f9960d
839 <8398a7@gmail.com>
2015-09-09 05:50:50 +0900

    refs #211 lockされるときにemailが空のユーザはメールを送信しない

---
+++ app/mailers/devise/mailer.rb
@@ -0,0 +1,18 @@
+class Devise::Mailer < Devise.parent_mailer.constantize
+  include Devise::Mailers::Helpers
+
+  def reset_password_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    return if record.email.empty?
+    @token = token
+    devise_mail(record, :unlock_instructions, opts)
+  end
+
+  def password_change(record, opts = {})
+    devise_mail(record, :password_change, opts)
+  end
+end
