4625062848c778a3da2c974a91d0b314578b3143
839 <8398a7@gmail.com>
2015-09-09 05:05:10 +0900

    refs #211 パスワード再発行メールを送信できるように設定

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,16 @@
+class Users::PasswordsController < Devise::PasswordsController
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      flash[:notice] = '再発行メールを送信しました'
+      flash[:alert] = '届いていない場合は迷惑メールフォルダをご確認下さい'
+    elsif !User.exists?(email: resource.email)
+      flash[:alert] = '登録されていないメールアドレスです'
+    else
+      flash[:alert] = '何らかの不具合によりメールが送れませんでした．管理人に連絡下さい'
+    end
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
 
   # all visitor
   root 'welcomes#index'
