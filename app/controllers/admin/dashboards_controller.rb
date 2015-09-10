ff9c57e019eb51c24bf447559356e00e0e0c4d94
839 <8398a7@gmail.com>
2015-09-10 21:00:39 +0900

    memory関係の情報を追加

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,7 +4,7 @@ class Admin::DashboardsController < ApplicationController
 
   def index
     begin
-      @sidekiq = Process.getpgid(File.read("#{Rails.root}/tmp/pids/sidekiq.pid").chomp!.to_i)
+      @sidekiq = Process.getpgid(File.read("#{Rails.root}/tmp/pids/sidekiq.pid").to_i)
     rescue
       @sidekiq = false
     end
@@ -12,5 +12,9 @@ class Admin::DashboardsController < ApplicationController
     @email = User.where.not(email: '').count
     @message = Message.where(state: false).count
     @user = User.where(current_sign_in_at: Date.today..Date.today + 1).count
+    @app_memory = (`ps -o rss= -p #{Process.pid}`.to_i / 1024.0).round(2)
+    @sidekiq_memory = (`ps -o rss= -p #{@sidekiq + 1}`.to_i / 1024.0).round(2) if @sidekiq
+    @used_memory = (`ps aux | tail -n +2 | awk -F' ' '{sum += $6} END {print sum}'`.to_i / 1024.0).round(2)
+    @margin_memory = ENV['MEMORY'].to_i - @used_memory
   end
 end
