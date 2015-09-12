c92e38ff03594524c27d523d24c119bbf8377115
839 <8398a7@gmail.com>
2015-09-13 04:16:55 +0900

    add spec for dashboards

---
+++ spec/features/admin/users/dashboards_spec.rb
@@ -0,0 +1,25 @@
+feature 'ダッシュボードの閲覧' do
+  background do
+    create(:user, id: 1, role: 100)
+    login(User.find(1))
+    visit admin_dashboards_path
+  end
+
+  it 'sidekiqの状態が見れる' do
+    expect(page).to have_content('sidekiq')
+    expect(page).to have_content('inactive')
+  end
+  it 'email登録人数が見れる' do
+    expect(page).to have_content('email登録人数')
+  end
+  it '未読メッセージが見れる' do
+    expect(page).to have_content('未読メッセージ')
+    expect(page).to have_content('0件')
+  end
+  it 'used memoryが見れる' do
+    expect(page).to have_content('used memory')
+  end
+  it 'margin memoryが見れる' do
+    expect(page).to have_content('margin memory')
+  end
+end
