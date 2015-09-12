926e252dfe2451c0b8bddd5fef7a82d1d77ed3c3
839 <8398a7@gmail.com>
2015-09-13 04:03:19 +0900

    無意味な項目を削除

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -12,8 +12,6 @@ class Admin::DashboardsController < ApplicationController
     @email = User.where.not(email: '').count
     @message = Message.where(state: false).count
     @user = User.where(current_sign_in_at: Date.today..Date.today + 1).count
-    @app_memory = (`ps -o rss= -p #{Process.pid}`.to_i / 1024.0).round(2)
-    @sidekiq_memory = (`ps -o rss= -p #{@sidekiq}`.to_i / 1024.0).round(2) if @sidekiq
     @used_memory = (`ps aux | tail -n +2 | awk -F' ' '{sum += $6} END {print sum}'`.to_i / 1024.0).round(2)
     @margin_memory = (ENV['MEMORY'].to_i - @used_memory).round(2)
   end
