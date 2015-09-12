7d7c3cfec2e96c7fa689c036e842ef7dea318d55
839 <8398a7@gmail.com>
2015-09-13 08:03:06 +0900

    setting capistrano for bower

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,3 +30,16 @@ namespace :deploy do
   after :publishing, :restart
   after 'deploy:finished', 'airbrake:deploy'
 end
+
+namespace :bower do
+  desc 'Install bower'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+        execute :rake, 'bower:resolve CI=true'
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'
