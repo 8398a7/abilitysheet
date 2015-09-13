988969fe08ecce71aa229d95a3819a2f6b5bf706
839 <8398a7@gmail.com>
2015-09-13 22:17:30 +0900

    divide iidxme spec into mock and real

--- lib/scrape/iidxme.rb
+++ lib/scrape/iidxme.rb
@@ -11,17 +11,21 @@ module Scrape
     private
 
     def process(iidxid)
+      return false unless User.exists?(iidxid: iidxid)
       elems = data_get(iidxid)
       return false unless elems
       user_id = User.find_by(iidxid: iidxid).id
       Score.iidxme_async(user_id, elems['musicdata'])
     end
 
+    def search_api(iidxid)
+      uri = URI.parse("http://json.iidx.me/?name=#{iidxid}")
+      JSON.parse(Net::HTTP.get(uri))
+    end
+
     def user_id_search(iidxid)
       return false unless iidxid =~ /\A\d{4}-\d{4}\z/
-      uri    = URI.parse("http://json.iidx.me/?name=#{iidxid}")
-      res    = Net::HTTP.get(uri)
-      hash = JSON.parse(res)
+      hash = search_api(iidxid)
       return false unless hash['users'].count == 1
       hash['users'][0]['userid']
     end
--- spec/lib/scrape/iidxme_spec.rb
+++ spec/lib/scrape/iidxme_spec.rb
@@ -1,9 +1,9 @@
 describe Scrape::IIDXME do
   let(:user) { create(:user, id: 1, iidxid: iidxid) }
-  let(:iidxme) { Scrape::IIDXME.new }
 
-  context '正常系' do
-    describe '存在するIIDXIDで処理を行う場合' do
+  context 'real' do
+    let(:iidxme) { Scrape::IIDXME.new }
+    context '存在するIIDXIDで処理を行う場合' do
       let(:iidxid) { '8594-9652' }
       it '#async' do
         create(:sheet, id: 1, title: 'F')
@@ -12,50 +12,73 @@ describe Scrape::IIDXME do
         expect(Score.find(1).score).to eq nil
         expect(Score.find(1).bp).to eq nil
         expect(iidxme.async(user.iidxid)).to be_truthy
-        expect(Score.find(1).state).to eq 0
+        expect(Score.find(1).state).not_to eq 7
         expect(Score.find(1).score).not_to eq nil
         expect(Score.find(1).bp).not_to eq nil
       end
-      it '#process' do
-        expect(iidxme.send(:process, user.iidxid)).to be_truthy
+      context 'IIDXIDの書式が正しくない場合' do
+        let(:iidxids) { %w(1 1110) }
+        it '#async' do
+          iidxids.each { |iidxid| expect(iidxme.async(iidxid)).to be_falsy }
+        end
       end
-      it '#user_id_search' do
-        expect(iidxme.send(:user_id_search, user.iidxid).class).to eq String
-      end
-      it '#data_get' do
-        expect(iidxme.send(:data_get, user.iidxid).class).to eq Hash
+      context '存在しないIIDXIDで処理を行う場合' do
+        let(:iidxid) { '0000-0000' }
+        it '#async' do
+          expect(iidxme.async(user.iidxid)).to be_falsy
+        end
       end
     end
   end
-  context '異常系' do
-    describe 'IIDXIDの書式が正しくない場合' do
-      let(:iidxids) { %w(1 1110) }
+
+  context 'mock' do
+    let(:iidxme_mock_root) { "#{Rails.root}/spec/mock/iidxme" }
+    before { @iidxme = Scrape::IIDXME.new }
+    context '存在するIIDXIDで処理を行う場合' do
+      let(:iidxid) { '8594-9652' }
+      before do
+        res = JSON.parse(File.read("#{iidxme_mock_root}/correct"))
+        allow(@iidxme).to receive(:data_get).and_return(res)
+      end
       it '#async' do
-        iidxids.each { |iidxid| expect(iidxme.async(iidxid)).to be_falsy }
-      end
-      it '#process' do
-        iidxids.each { |iidxid| expect(iidxme.send(:process, iidxid)).to be_falsy }
-      end
-      it '#user_id_search' do
-        iidxids.each { |iidxid| expect(iidxme.send(:user_id_search, iidxid)).to be_falsy }
-      end
-      it '#data_get' do
-        iidxids.each { |iidxid| expect(iidxme.send(:data_get, iidxid)).to be_falsy }
+        create(:sheet, id: 1, title: 'F')
+        create(:score, id: 1, user_id: 1, sheet_id: 1)
+        expect(Score.find(1).state).to eq 7
+        expect(Score.find(1).score).to eq nil
+        expect(Score.find(1).bp).to eq nil
+        expect(@iidxme.async(user.iidxid)).to be_truthy
+        expect(Score.find(1).state).to eq 0
+        expect(Score.find(1).score).to eq 2994
+        expect(Score.find(1).bp).to eq 2
       end
     end
-    describe '存在しないIIDXIDで処理を行う場合' do
-      let(:iidxid) { '0000-0000' }
+    context 'IIDXIDの書式が正しくない場合' do
+      let(:iidxids) { %w(1 1110) }
+      before do
+        res = JSON.parse(File.read("#{iidxme_mock_root}/correct"))
+        allow(@iidxme).to receive(:data_get).and_return(res)
+      end
       it '#async' do
-        expect(iidxme.async(user.iidxid)).to be_falsy
+        iidxids.each { |iidxid| expect(@iidxme.async(iidxid)).to be_falsy }
+      end
+    end
+    context '存在しないIIDXIDで処理を行う場合' do
+      let(:iidxid) { '0000-0000' }
+      before do
+        res = JSON.parse(File.read("#{iidxme_mock_root}/not_found"))
+        allow(@iidxme).to receive(:search_api).and_return(res)
+      end
+      it '#async' do
+        expect(@iidxme.async(user.iidxid)).to be_falsy
       end
       it '#process' do
-        expect(iidxme.send(:process, user.iidxid)).to be_falsy
+        expect(@iidxme.send(:process, user.iidxid)).to be_falsy
       end
       it '#user_id_search' do
-        expect(iidxme.send(:user_id_search, user.iidxid)).to be_falsy
+        expect(@iidxme.send(:user_id_search, user.iidxid)).to be_falsy
       end
       it '#data_get' do
-        expect(iidxme.send(:data_get, user.iidxid)).to be_falsy
+        expect(@iidxme.send(:data_get, user.iidxid)).to be_falsy
       end
     end
   end
