8373fdef71866d46618518836b3e12a56a49e627
839 <8398a7@gmail.com>
2015-09-14 12:27:02 +0900

    fix error with recommends controller spec

--- spec/controllers/recommends_controller_spec.rb
+++ spec/controllers/recommends_controller_spec.rb
@@ -1,19 +1,6 @@
 describe RecommendsController, type: :controller do
-  describe 'GET #list' do
-    before { get :list }
-    it 'response ok' do
-      expect(response).to have_http_status(:success)
-    end
-    it 'assigns the page as @color' do
-      expect(assigns(:color)).to eq Static::COLOR
-    end
-    it 'assigns the page as @sheets' do
-      expect(assigns(:sheets)).to eq Sheet.active.preload(:ability)
-    end
-  end
-
-  describe 'GET #integration' do
-    before { get :integration }
+  describe 'GET #index' do
+    before { get :index }
     it 'response ok' do
       expect(response).to have_http_status(:success)
     end
