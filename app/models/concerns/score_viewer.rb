0fa4feb68336e1f5191dfb49350a220da69a3d53
839 <8398a7@gmail.com>
2015-09-20 06:56:05 +0900

    score viewer同期時にランプが上書きされる不具合を修正

--- app/models/concerns/score_viewer.rb
+++ app/models/concerns/score_viewer.rb
@@ -4,7 +4,7 @@ module ScoreViewer
   included do
     def self.api_score_viewer(elems, current_user)
       elems.each do |e|
-        score_params = { 'sheet_id' => e['id'], 'state' => e['cl'], 'score' => e['pg'] * 2 + e['g'], 'bp' => e['miss'] }
+        score_params = { 'sheet_id' => e['id'], 'score' => e['pg'] * 2 + e['g'], 'bp' => e['miss'] }
         score_params['score'] = nil if e['pg'] == -1 && e['g'] == -1
         score_params['bp'] = nil if e['miss'] == -1
 
@@ -12,6 +12,7 @@ module ScoreViewer
         score_params['score'] = e['pg'] * 2 if e['pg'] != -1 && e['g'] == -1
 
         score = Score.find_by(user_id: current_user.id, sheet_id: e['id'])
+        score_params['state'] = e['cl'] if e['cl'].to_i < score.state
         score.update_with_logs(score_params)
       end
     end
