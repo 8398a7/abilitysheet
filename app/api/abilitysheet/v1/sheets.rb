d626888fbab0f77fba68b30ed9001b8abef98afb
839 <8398a7@gmail.com>
2015-10-08 20:34:46 +0900

    楽曲情報を公開するAPIを追加

--- app/api/abilitysheet/v1/sheets.rb
+++ app/api/abilitysheet/v1/sheets.rb
@@ -2,8 +2,7 @@ module Abilitysheet::V1
   class Sheets < Grape::API
     resource :sheets do
       get '' do
-        doorkeeper_authorize!
-        Sheet.select(:id, :title, :n_ability, :h_ability, :version, :active, :textage)
+        { sheets: Sheet.order(:id) }
       end
     end
   end
