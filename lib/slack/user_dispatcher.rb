0574175afa7aab6a674bee12b8e16ee2eab8f2af
839 <8398a7@gmail.com>
2015-11-07 12:47:21 +0900

    削除者を通知する

--- lib/slack/user_dispatcher.rb
+++ lib/slack/user_dispatcher.rb
@@ -16,5 +16,22 @@ module Slack
       }
       Net::HTTP.post_form(Abilitysheet::Application::SLACK_URI, payload: body.to_json)
     end
+
+    def self.delete_user_notify(id)
+      user = User.find(id)
+      body = {
+        username: 'User Dispatcher',
+        attachments: [
+          {
+            color: :danger,
+            title: '削除者',
+            pretext: '_delete user_',
+            text: "現在の登録者: #{User.count}人\n削除者: [#{user.iidxid}] #{user.djname} <#{Static::PREF[user.pref]}, #{Static::GRADE[user.grade]}>",
+            mrkdwn_in: %w(pretext)
+          }
+        ]
+      }
+      Net::HTTP.post_form(Abilitysheet::Application::SLACK_URI, payload: body.to_json)
+    end
   end
 end
