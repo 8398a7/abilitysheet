2dd624f3cd158dd34c73ba9e68f4f87246627f05
839 <8398a7@gmail.com>
2015-12-20 06:25:31 +0900

    remove relative url root

--- config/initializers/jsroutes.rb
+++ config/initializers/jsroutes.rb
@@ -1,3 +1,3 @@
 JsRoutes.setup do |config|
-  config.prefix = '/abilitysheet' if Rails.env.staging? || Rails.env.production?
+  # config.prefix = '/abilitysheet' if Rails.env.staging? || Rails.env.production?
 end
