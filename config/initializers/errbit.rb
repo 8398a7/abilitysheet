38cc0121aae696d4390268f2d3f4b4b4d6eca0e5
839 <8398a7@gmail.com>
2015-12-30 00:47:24 +0900

    airbrakeをproductionの時にのみ有効にする

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,7 @@
-Airbrake.configure do |config|
-  config.project_id  = ENV['ERRBIT_API_KEY']
-  config.project_key = ENV['ERRBIT_API_KEY']
-  config.host        = ENV['ERRBIT_HOST']
+if Rails.env.production?
+  Airbrake.configure do |config|
+    config.project_id  = ENV['ERRBIT_API_KEY']
+    config.project_key = ENV['ERRBIT_API_KEY']
+    config.host        = ENV['ERRBIT_HOST']
+  end
 end
