b556a978948522e6f201c4d859178cfd092aa83e
839 <8398a7@gmail.com>
2016-01-02 18:04:06 +0900

    refactoring user model

---
+++ app/models/concerns/user/devise_methods.rb
@@ -0,0 +1,32 @@
+module User::DeviseMethods
+  extend ActiveSupport::Concern
+
+  included do
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      if login
+        find_by('username = :value OR iidxid = :value', value: login)
+      else
+        find_by(conditions)
+      end
+    end
+
+    def update_without_current_password(params, *options)
+      params.delete(:current_password)
+      params.delete(:password) if params[:password].blank?
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+
+      clean_up_passwords
+      update_attributes(params, *options)
+    end
+
+    def email_required?
+      false
+    end
+
+    def email_changed?
+      false
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,77 +28,29 @@
 #
 
 class User < ActiveRecord::Base
-  has_many :scores, dependent: :delete_all
-  has_many :logs, dependent: :delete_all
-  has_many :follows, dependent: :delete_all
-
   devise :database_authenticatable, :registerable, :rememberable, :recoverable, :trackable, :validatable, :lockable
   attr_accessor :login
 
-  include List
+  include User::DeviseMethods
   include User::FollowMethods
+  include User::List
+  include User::Role
+
+  has_many :scores, dependent: :delete_all
+  has_many :logs, dependent: :delete_all
+  has_many :follows, dependent: :delete_all
 
   # usernameを必須・一意とする
   validates_uniqueness_of :username, :iidxid, :email
   validates_presence_of :username, :djname, :iidxid, :grade, :pref, :email
 
-  validates :iidxid, format: {
-    with: /\A\d{4}-\d{4}\z/,
-    message: 'が正しくありません。'
-  }, length: {
-    is: 9
-  }
-  validates :grade, numericality: {
-    only_integer: true
-  }, inclusion: {
-    in: Abilitysheet::Application.config.iidx_grade..18, message: 'のパラメタが異常です。'
-  }
-  validates :pref, numericality: {
-    only_integer: true
-  }, inclusion: {
-    in: 0..47, message: 'のパラメタが異常です。'
-  }
+  validates :iidxid, format: { with: /\A\d{4}-\d{4}\z/, message: 'が正しくありません。' }, length: { is: 9 }
+  validates :grade, numericality: { only_integer: true }, inclusion: { in: Abilitysheet::Application.config.iidx_grade..18, message: 'のパラメタが異常です。' }
+  validates :pref, numericality: { only_integer: true }, inclusion: { in: 0..47, message: 'のパラメタが異常です。' }
   validates :djname, length: { maximum: 6 }, format: { with: /\A[A-Z0-9\-\_.*!#&]+\z/, message: 'は半角大文字英字で記入して下さい' }
   validates :username, length: { maximum: 15 }, format: { with: /\A[a-z_0-9]+\z/, message: 'は半角英数字で記入して下さい' }
 
   scope :search_djname, ->(query) { User.where(['djname LIKE ?', "%#{PGconn.escape(query)}%"]) }
-
-  module Special
-    USERS = [1, 2, 3, 4, 5, 6, 8, 13, 16, 21, 34, 53, 63, 73, 100]
-  end
-
-  module Role
-    OWNER = 100
-    ADMIN = 75
-    MEMBER = 50
-    SPECIAL = 25
-    GENERAL = 0
-  end
-
-  def owner?
-    Role::OWNER <= role
-  end
-
-  def admin?
-    Role::ADMIN <= role
-  end
-
-  def member?
-    Role::MEMBER <= role
-  end
-
-  def special?
-    Role::SPECIAL == role || Role::OWNER == role
-  end
-
-  def update_without_current_password(params, *options)
-    params.delete(:current_password)
-    params.delete(:password) if params[:password].blank?
-    params.delete(:password_confirmation) if params[:password_confirmation].blank?
-
-    clean_up_passwords
-    update_attributes(params, *options)
-  end
 
   class << self
     def dan
@@ -113,30 +65,6 @@ class User < ActiveRecord::Base
       array
     end
 
-    def find_first_by_auth_conditions(warden_conditions)
-      conditions = warden_conditions.dup
-      login = conditions.delete(:login)
-      if login
-        find_by('username = :value OR iidxid = :value', value: login)
-      else
-        find_by(conditions)
-      end
-    end
-
-    def recent200
-      users = ActiveRecord::Base.connection.execute('SELECT users.id, users.djname, users.iidxid, users.pref, scores.updated_at, scores.state, users.grade, sheets.title  FROM users, scores, sheets WHERE users.id = scores.user_id AND scores.state != 7 AND sheets.id = scores.sheet_id ORDER BY scores.updated_at DESC LIMIT 6400').to_a
-      recent_users = []
-      ret = []
-      users.each do |user|
-        break if 200 <= recent_users.size
-        next if recent_users.include?(user['id'])
-        recent_users.push(user['id'])
-        user['updated_at'] = user['updated_at'].split[0]
-        ret.push(user)
-      end
-      ret
-    end
-
     def version_up
       User.includes(:scores).find_each do |user|
         version_up_score_create(user, user.scores)
@@ -149,14 +77,5 @@ class User < ActiveRecord::Base
         user.scores.create(sheet_id: score.sheet_id, state: score.state, version: Abilitysheet::Application.config.iidx_version)
       end
     end
-  end
-
-  # 登録時にemailを不要とする
-  def email_required?
-    false
-  end
-
-  def email_changed?
-    false
   end
 end
