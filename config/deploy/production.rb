4c81f5bd7db95041b452cae44e2fe5fad3af6c6c
839 <8398a7@gmail.com>
2016-01-07 07:56:48 +0900

    PORTを指定できるように修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,11 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :migration_role, 'db'
 
-server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
+if ENV['PRODUCTION_PORT']
+  server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], port: ENV['PRODUCTION_PORT'], roles: %w(web app db)
+else
+  server ENV['PRODUCTION_SERVER'], user: ENV['PRODUCTION_USER'], roles: %w(web app db)
+end
 
 set :ssh_options, {
   keys: [File.expand_path(ENV['RSA_KEY'])],
