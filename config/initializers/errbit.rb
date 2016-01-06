66d4d2ced669170e8a8b42d1ea92a12682cb806c
839 <8398a7@gmail.com>
2016-01-07 04:46:05 +0900

    follow errbit config

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.project_id  = ENV['ERRBIT_API_KEY']
-  config.project_key = ENV['ERRBIT_API_KEY']
   config.host        = ENV['ERRBIT_HOST']
+  config.project_id  = true
+  config.project_key = ENV['ERRBIT_API_KEY']
 end
