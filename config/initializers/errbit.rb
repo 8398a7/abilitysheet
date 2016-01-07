d06b8737b80aa3717dc3e0939abd64c26a469c65
839 <8398a7@gmail.com>
2016-01-07 09:10:00 +0900

    エラー通知の際にrevisionを付与する

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,19 @@
+def get_revision
+  return '' if Rails.env.development? || Rails.env.test?
+  path = Rails.root.to_s.split('/')
+  version = path.pop
+  path.pop
+  revisions = path.join('/') + '/revisions.log'
+  revision = nil
+  File.read(revisions).split("\n").each do |line|
+    next unless line.include?(version)
+    revision = line.match(/[a-z0-9]{7}/)[0]
+  end
+  '@' + revision
+end
+
 Airbrake.configure do |config|
-  config.environment = Rails.env
+  config.environment = Rails.env + get_revision
   config.ignore_environments = %w(development test)
   config.host        = ENV['ERRBIT_HOST']
   config.project_id  = true
