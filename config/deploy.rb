a2202387c8baa6fe1aa33a20ebad2fbb8124b8c2
839 <8398a7@gmail.com>
2016-01-09 08:34:12 +0900

    backup dirをlinked

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 set :linked_files, fetch(:linked_files, []).push('.env')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'vendor/assets/bower_components')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/backup', 'vendor/bundle', 'vendor/assets/bower_components')
 set :keep_releases, 5
 
 set :default_env, path: '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
