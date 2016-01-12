49a5632cdc5661d7411a9ce05a6f3d72ff8a088c
839 <8398a7@gmail.com>
2016-01-12 16:05:21 +0900

    refactoring users::registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,28 +1,26 @@
-module Users
-  class RegistrationsController < Devise::RegistrationsController
-    def create
-      super
-      sync_score
-    end
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    super
+    sync_score
+  end
 
-    def destroy
-      Slack::UserDispatcher.delete_user_notify(current_user.id)
-      super
-    end
+  def destroy
+    Slack::UserDispatcher.delete_user_notify(current_user.id)
+    super
+  end
 
-    private
+  private
 
-    def sync_score
-      iidxid = env['rack.request.form_hash']['user']['iidxid']
-      user_id = User.find_by(iidxid: iidxid).try(:id)
-      return unless user_id
-      Slack::UserDispatcher.new_register_notify(user_id)
-      unless SidekiqDispatcher.exists?
-        Slack::SidekiqDispatcher.notify
-        return
-      end
-      ManagerWorker.perform_async(user_id)
-      IidxmeWorker.perform_async(user_id)
+  def sync_score
+    iidxid = env['rack.request.form_hash']['user']['iidxid']
+    user_id = User.find_by(iidxid: iidxid).try(:id)
+    return unless user_id
+    Slack::UserDispatcher.new_register_notify(user_id)
+    unless SidekiqDispatcher.exists?
+      Slack::SidekiqDispatcher.notify
+      return
     end
+    ManagerWorker.perform_async(user_id)
+    IidxmeWorker.perform_async(user_id)
   end
 end
