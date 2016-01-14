623e32170373a640cc4c2aa330ab4f4b9b7fe52d
839 <8398a7@gmail.com>
2016-01-14 11:06:23 +0900

    retryとretry_waitの設定を見直し

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
   config.around :each do |ex|
-    ex.run_with_retry retry: 3
+    ex.run_with_retry retry: 10
   end
 
   Sidekiq::Testing.inline!
