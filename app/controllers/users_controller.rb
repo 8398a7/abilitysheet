54d451e40bbb7ab41dd063213925c98f7f6be076
839 <8398a7@gmail.com>
2016-01-16 10:46:07 +0900

    emailチェックを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @cnt = User.select(:id).count
+    @title = '最近更新した200人'
     if params[:query] && params[:query].present?
       @users = User.search_djname(params[:query].upcase)
       @scores_map = User.users_list(:rivals, @users)
