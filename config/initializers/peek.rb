26558b2dfab66ea825a097e03f6754378076e32f
839 <8398a7@gmail.com>
2016-02-02 20:38:09 +0900

    refs #459 peek関係のgemを整理

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -1,28 +1,7 @@
-def get_revision
-  return '' if Rails.env.development? || Rails.env.test?
-  path = Rails.root.to_s.split('/')
-  version = path.pop
-  path.pop
-  revisions = path.join('/') + '/revisions.log'
-  revision = nil
-  File.read(revisions).split("\n").each do |line|
-    next unless line.include?(version)
-    revision = line.match(/[a-z0-9]{7}/)[0]
-  end
-  revision
-rescue
-  return ''
-end
-
-Peek.into Peek::Views::GC
-if Rails.env.production?
-  Peek.into Peek::Views::Git, nwo: '8398a7/abilitysheet', branch_name: 'master', protocol: 'https', domain: 'github.com', sha: get_revision
-else
-  Peek.into Peek::Views::Git, nwo: '8398a7/abilitysheet'
-end
-Peek.into Peek::Views::Host
 Peek.into Peek::Views::PerformanceBar
-Peek.into Peek::Views::PG
 Peek.into Peek::Views::Rblineprof
-Peek.into Peek::Views::Redis
-Peek.into Peek::Views::Sidekiq
+if Rails.env.development?
+  Peek.into Peek::Views::GC
+  Peek.into Peek::Views::Git, nwo: '8398a7/abilitysheet'
+  Peek.into Peek::Views::PG
+end
