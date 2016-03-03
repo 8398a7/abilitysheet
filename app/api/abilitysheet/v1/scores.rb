824938954ee132524fbc66f6b05225c1c5d6f783
839 <8398a7@gmail.com>
2016-03-03 22:59:28 +0900

    IIDXME更新用のAPIを設置

--- app/api/abilitysheet/v1/scores.rb
+++ app/api/abilitysheet/v1/scores.rb
@@ -1,8 +1,13 @@
 module Abilitysheet::V1
   class Scores < Grape::API
     resources :scores do
-      post 'official_sync' do
-        authenticate!
+      params do
+        requires :iidxid, type: String, desc: 'iidxid'
+      end
+      post 'sync/iidxme/:iidxid' do
+        user = User.find_by(iidxid: params[:iidxid])
+        error! '404 Not Found', 404 unless user
+        IidxmeWorker.perform_async(user.id)
         { result: :ok }
       end
     end
