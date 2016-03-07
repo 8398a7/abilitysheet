2626a3883daaf2a318a091dbe3a6e65abcbf9c47
839 <8398a7@gmail.com>
2016-03-07 23:52:33 +0900

    メソッド名が誤っていたのを修正

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ScoresController < Api::V1::BaseController
-  def index
+  def sync_iidxme
     user = User.find_by!(iidxid: params[:iidxid])
     IidxmeWorker.perform_async(user.id)
     render json: { result: :ok, date: Date.today }
