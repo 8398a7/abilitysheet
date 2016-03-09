9988b9f3f230ec9cdbb96d6a422f7f4ae81c8dbe
839 <8398a7@gmail.com>
2016-03-10 00:26:26 +0900

    refs #480 ユーザのグラフを生成するAPIのロジックをモデルに移譲

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -17,16 +17,6 @@ class Api::V1::LogsController < Api::V1::BaseController
     user = User.find_by_iidxid!(params[:iidxid])
     end_month = "#{params[:year]}-#{params[:month]}-01".to_date + 1.month
     start_month = end_month - 2.months
-    categories = user.logs.create_between(start_month, end_month).map { |b| (b[0] - 1.month).to_s.slice(0, 7) }
-    column = user.logs.column(start_month, end_month)
-    spline = user.logs.spline(start_month, end_month)
-    render json: {
-      categories: categories,
-      pie: user.scores.is_active.is_current_version.pie,
-      column: column,
-      column_max: column.flatten.max,
-      spline: spline,
-      spline_max: spline.flatten.max
-    }
+    render json: user.graph(start_month, end_month)
   end
 end
--- app/models/concerns/user/api.rb
+++ app/models/concerns/user/api.rb
@@ -2,6 +2,20 @@ module User::API
   extend ActiveSupport::Concern
 
   included do
+    def graph(start_month, end_month)
+      categories = logs.create_between(start_month, end_month).map { |b| (b[0] - 1.month).to_s.slice(0, 7) }
+      column = logs.column(start_month, end_month)
+      spline = logs.spline(start_month, end_month)
+      render json: {
+        categories: categories,
+        pie: scores.is_active.is_current_version.pie,
+        column: column,
+        column_max: column.flatten.max,
+        spline: spline,
+        spline_max: spline.flatten.max
+      }
+    end
+
     def schema
       {
         id: id,
