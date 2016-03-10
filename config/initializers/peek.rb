965c337c8ffc684d544b9f8859bf25864cc53660
839 <8398a7@gmail.com>
2016-03-10 19:23:27 +0900

    production環境で表示するpeekの情報を追加

--- config/initializers/peek.rb
+++ config/initializers/peek.rb
@@ -3,3 +3,5 @@ Peek.into Peek::Views::Rblineprof
 Peek.into Peek::Views::GC
 Peek.into Peek::Views::Git, nwo: '8398a7/abilitysheet'
 Peek.into Peek::Views::PG
+Peek.into Peek::Views::Redis
+Peek.into Peek::Views::Sidekiq
