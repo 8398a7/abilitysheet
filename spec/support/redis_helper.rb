c3fc8cb688892df70befecc6f02f8a285ac97066
839 <8398a7@gmail.com>
2016-03-24 06:27:30 +0900

    本番の楽曲状態を同期するAPIのパスを変更

--- spec/support/redis_helper.rb
+++ spec/support/redis_helper.rb
@@ -1,7 +1,7 @@
 module RedisHelper
   def self.load_sheets_data
     redis = Redis.new
-    uri = URI.parse('https://iidx12.tk/api/v1/sheets')
+    uri = URI.parse('https://iidx12.tk/api/v1/sheets/list')
     sheets = JSON.parse(Net::HTTP.get(uri))
     redis.set('sheets', sheets.to_json)
   rescue
