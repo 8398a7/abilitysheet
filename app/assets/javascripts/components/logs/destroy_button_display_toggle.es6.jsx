19204f7a9197f41d1b2dda9d6c7588799c06d27d
839 <8398a7@gmail.com>
2016-03-26 05:57:40 +0900

    refs #559 ログ周りをES6化

---
+++ app/assets/javascripts/components/logs/destroy_button_display_toggle.es6.jsx
@@ -0,0 +1,41 @@
+class DestroyButtonDisplayToggle extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      nonDisplay: true,
+      display: false
+    }
+  }
+
+  toggleDisplay(type) {
+    $('.destroy-button').each((index, elem) => type === 'display' ? $(elem).show() : $(elem).hide())
+  }
+
+
+  onClick(type) {
+    this.toggleDisplay(type)
+    switch (type) {
+      case 'nonDisplay':
+        this.setState({
+          nonDisplay: true,
+          display: false
+        })
+        break
+      case 'display':
+        this.setState({
+          nonDisplay: false,
+          display: true
+        })
+        break
+    }
+  }
+
+  render() {
+    return (
+      <div className='uk-button-group' style={{marginBottom: '10px'}}>
+        <button onClick={() => this.onClick('nonDisplay')} className={'uk-button uk-button-primary' + (this.state.nonDisplay ? ' uk-active' : '')}>非表示</button>
+        <button onClick={() => this.onClick('display')} className={'uk-button uk-button-danger' + (this.state.display ? ' uk-active' : '')}>表示</button>
+      </div>
+    )
+  }
+}
