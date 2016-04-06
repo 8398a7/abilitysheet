455b03cc15f2f72d363527eab33420bfda0b4528
839 <8398a7@gmail.com>
2016-04-06 22:52:48 +0900

    fix typo

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,22 +1,27 @@
 class Api::V1::LogsController < Api::V1::BaseController
+  before_action :load_user!
+
   def full_calendar
-    user = User.find_by!(iidxid: params[:iidxid])
     start_date = (params[:year].to_s + '-' + params[:month].to_s + '-01').to_date
     end_date = start_date + 1.month
-    logs = user.logs.where(created_date: start_date..end_date)
+    logs = @user.logs.where(created_date: start_date..end_date)
     render json: logs.preload(:sheet).map(&:schema)
   end
 
   def cal_heatmap
-    user = User.find_by!(iidxid: params[:iidxid])
-    logs = user.logs.where(created_date: params[:start]..params[:stop]).map { |c| c.created_date.to_time.to_i }
+    logs = @user.logs.where(created_date: params[:start]..params[:stop]).map { |c| c.created_date.to_time.to_i }
     render json: logs.each_with_object(Hash.new(0)) { |tm, h| h[tm] += 1 }
   end
 
   def graph
-    user = User.find_by_iidxid!(params[:iidxid])
     end_month = "#{params[:year]}-#{params[:month]}-01".to_date + 1.month
     start_month = end_month - 2.months
-    render json: user.graph(start_month, end_month)
+    render json: @user.graph(start_month, end_month)
+  end
+
+  private
+
+  def load_user!
+    @user = User.find_by_iidxid!(params[:iidxid])
   end
 end
