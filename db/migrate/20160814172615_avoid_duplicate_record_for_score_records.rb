da0bb693412d1b23996a6baecdde61b0d276a909
839 <8398a7@gmail.com>
2016-08-15 05:33:32 +0900

    [ci skip] 削除するスコアレコードを表示する

--- db/migrate/20160814172615_avoid_duplicate_record_for_score_records.rb
+++ db/migrate/20160814172615_avoid_duplicate_record_for_score_records.rb
@@ -1,7 +1,9 @@
 class AvoidDuplicateRecordForScoreRecords < ActiveRecord::Migration
   def change
     Score.find_each do |score|
-      score.destroy unless score.valid?
+      next if score.valid?
+      p score
+      score.destroy
     end
     add_index :scores, %i(version sheet_id user_id), unique: true
   end
