37e3902f599cac6c3064af5f804575f0cd3400a5
839 <8398a7@gmail.com>
2016-08-15 02:25:18 +0900

    refs #670 重複したスコアレコードが作らないためのテストを追加

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -96,11 +96,20 @@ describe Score, type: :model do
   end
 
   context 'validate' do
-    it '一つのバージョンでユーザは同じ楽曲を複数持たない' do
-      expect do
-        Score.create(id: 99, user_id: 99, sheet_id: 1, version: 1)
-        Score.create(id: 100, user_id: 99, sheet_id: 1, version: 1)
-      end.to change(Score, :count).by(1)
+    context '一つのバージョンでユーザは同じ楽曲を複数持たない' do
+      it 'rails側のvalidationに引っかかること' do
+        expect do
+          Score.create(id: 99, user_id: 99, sheet_id: 1, version: 1)
+          Score.create(id: 100, user_id: 99, sheet_id: 1, version: 1)
+        end.to change(Score, :count).by(1)
+      end
+      it 'rdb側のvalidationに引っかかること' do
+        expect do
+          Score.create(id: 99, user_id: 99, sheet_id: 1, version: 1)
+          score = Score.new(id: 100, user_id: 99, sheet_id: 1, version: 1)
+          score.save(validate: false)
+        end.to change(Score, :count).by(1)
+      end
     end
   end
 end
