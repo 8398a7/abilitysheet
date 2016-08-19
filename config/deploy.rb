f670a4193e04dd463ed741746e0a5d86f59cce5f
839 <8398a7@gmail.com>
2016-08-19 19:01:03 +0900

    refs #687 assets_rails taskを移行

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,15 +45,5 @@ set :puma_threads, [8, 32]
 set :puma_workers, 3
 set :puma_worker_timeout, 15
 
-namespace :assets_rails do
-  desc 'Install assets and resolve assets'
-  task :install do
-    on roles(:web) do
-      within release_path do
-        execute :rake, 'assets_rails:install assets_rails:resolve RAILS_ENV=production'
-      end
-    end
-  end
-end
 before 'puma:check', 'puma:config'
 before 'deploy:compile_assets', 'assets_rails:install'
