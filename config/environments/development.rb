0141ddcb04dd93ede97dd9faa80487262ee12acd
839 <8398a7@gmail.com>
2016-09-08 23:05:22 +0900

    refs #712 remove duplicate mail setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.raise_delivery_errors = true
+  #
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'localhost',
