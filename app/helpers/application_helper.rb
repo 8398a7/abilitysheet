f0673b3f9ef36dc77fe95fb0c76d397043002454
839 <8398a7@gmail.com>
2016-09-19 22:54:35 +0900

    [#738] ノマゲ/ハード表のトップページに最近の更新リンクを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,11 @@ module ApplicationHelper
     return true unless current_user
     !(current_user.special? || current_user.owner?)
   end
+
+  def recent_link(iidxid)
+    user = User.find_by_iidxid(iidxid)
+    return false unless user
+    return false if user.logs.empty?
+    logs_path(user.iidxid, user.logs.order(:created_date).last.created_date)
+  end
 end
