26e0950cbc4852fbcdf4dd34938d8b8bf371087b
839 <8398a7@gmail.com>
2016-12-26 23:31:24 +0900

    [#810] notified of all exceptions and setting of logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
+
+logger = ::Logger.new('log/raven.log')
+logger.level = ::Logger::DEBUG
+
 Raven.configure do |config|
+  config.logger = logger
+  config.excluded_exceptions = []
   config.dsn = ENV['SENTRY_DSN']
   config.environments = %w(staging production)
 end
