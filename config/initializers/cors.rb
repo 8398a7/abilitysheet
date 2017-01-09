713bf98b7f7e2569bbff9e6e8bb07089b30d0930
839 <8398a7@gmail.com>
2017-01-09 18:43:12 +0900

    [#599] restrict cors information

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :options]
+    origins 'p.eagate.573.jp'
+    resource '*', headers: :any, methods: :post
   end
 end
