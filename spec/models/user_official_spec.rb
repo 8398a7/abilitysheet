d494451e0c7e8b8006c41b8290596854a6d37fe9
839 <8398a7@gmail.com>
2017-01-15 00:55:54 +0900

    [#599] create a test template

---
+++ spec/models/user_official_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+# == Schema Information
+#
+# Table name: follows
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer
+#  target_user_id :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+describe User::Official, type: :model do
+  describe '#update_official' do
+    let(:user) { create(:user) }
+    let(:params) { JSON.parse(File.read("#{Rails.root}/spec/mock/official.json")).deep_symbolize_keys }
+    it '同期できる' do
+      sync_sheet
+      expect(user.update_official(params)).to eq true
+      expect(user.scores.pluck(:state).size).to eq Sheet.active.count
+    end
+  end
+end
