3606f3126b803ab769b328482d77f1297633d419
839 <8398a7@gmail.com>
2017-01-27 01:11:20 +0900

    [#801] changed to scraping dom from api

--- lib/scrape/iidxme.rb
+++ lib/scrape/iidxme.rb
@@ -4,9 +4,15 @@ module Scrape
     GRADE_MAX = 20
 
     def initialize
-      @agent = Mechanize.new
-      @iidxme_domain = 'https://iidx.me'
-      @json_iidxme_domain = 'http://json.iidx.me'
+      @iidxme_domain = 'http://iidx.me'
+      @result = {
+        'userdata' => {
+          'djname' => nil,
+          'spclass' => nil
+        },
+        'musicdata' => [],
+        'image' => nil
+      }
     end
 
     def sync(iidxid)
@@ -16,29 +22,43 @@ module Scrape
 
     private
 
-    def download_profile_image(user)
-      User.find(user.id).remove_image!
-      file = open("#{@iidxme_domain}/userdata/copula/#{user.iidxid.delete('-')}/qpro.png?t=0")
+    def download_profile_image(elems, user)
+      user.remove_image!
+      file = open("#{@iidxme_domain}#{elems['userdata']['image']}")
       return nil if file.class != Tempfile
       file
     end
 
     def process(iidxid)
       return false unless User.exists?(iidxid: iidxid)
-      elems = data_get(iidxid)
+      elems = get_data(iidxid)
       return false unless elems
       user = User.find_by(iidxid: iidxid)
       user.update!(
         djname: elems['userdata']['djname'],
         grade: (elems['userdata']['spclass'] - GRADE_MAX).abs,
-        image: download_profile_image(user)
+        image: download_profile_image(elems, user)
       )
       Score.iidxme_sync(user.id, elems['musicdata'])
     end
 
     def search_api
-      uri = URI.parse("#{@json_iidxme_domain}/!/userlist")
-      JSON.parse(Net::HTTP.get(uri), symbolize_names: true)
+      result = { users: [] }
+      page = 1
+      loop do
+        html = parser("#{@iidxme_domain}/!/userlist?page=#{page}")
+        break if html.xpath('//div[@class="table userlist"]/div').size == 1
+        html.xpath('//div[@class="table userlist"]/div').each do |div|
+          next if div.xpath('div[@class="td iidxid"]').empty?
+          user_link = div.xpath('div[@class="td djname"]/div/a')[0]['href']
+          result[:users].push(
+            iidxid: div.xpath('div[@class="td iidxid"]').text.strip,
+            userid: user_link[1..user_link.size]
+          )
+        end
+        page += 1
+      end
+      result
     end
 
     def user_id_search(iidxid)
@@ -48,12 +68,44 @@ module Scrape
       user
     end
 
+    def parser(url)
+      dom = Net::HTTP.get(URI.parse(url))
+      Nokogiri::HTML.parse(dom, nil, 'UTF-8')
+    end
+
+    def get_userdata
+      html = parser("#{@iidxme_domain}/#{user_id}/sp/level/12")
+      @result['userdata']['djname'] ||= html.xpath('//div[@class="djname"]').text.strip
+      @result['userdata']['spclass'] ||= html.xpath('//div[@class="spclass"]/a')[0]['href'].split('?sp=')[1].to_i
+      @result['userdata']['image'] ||= html.xpath('//div[@class="qpro"]/a/img')[0]['src']
+      true
+    end
+
     def data_get(iidxid)
       user_id = user_id_search(iidxid)
       return false unless user_id
-      uri    = URI.parse("#{@json_iidxme_domain}/#{user_id}/sp/level/12/")
-      res    = Net::HTTP.get(uri)
-      JSON.parse(res)
+      page = 1
+      loop do
+        html = parser("#{@iidxme_domain}/#{user_id}/sp/level/12?page=#{page}")
+        break if html.xpath('//div[@class="table musiclist"]/div/div/div').text == 'NO RESULT'
+        html.xpath('//div[@class="table musiclist"]/div').each do |div|
+          next if div.xpath('div[@class="td title"]').empty?
+          score = div.xpath('div[@class="td score"]/div/span')[0]
+          score = score ? score.text.to_i : nil
+          miss = div.xpath('div[@class="td miss"]/div').text
+          miss = miss == '-' ? nil : miss.to_i
+          @result['musicdata'].push(
+            'data' => {
+              'title' => div.xpath('div[@class="td title"]').text
+            },
+            'clear' => div.xpath('div[@class="td clear"]/div/a/span')[0]['class'].split[1].delete('clear').to_i || 0,
+            'score' => score,
+            'miss' => miss
+          )
+        end
+        page += 1
+      end
+      @result
     end
   end
 end
