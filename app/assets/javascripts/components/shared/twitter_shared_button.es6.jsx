6f2264ae939632db3814ff9c560a3ebf369f4d84
839 <8398a7@gmail.com>
2017-02-04 18:06:29 +0900

    fixed bug of URL to share on twitter

--- app/assets/javascripts/components/shared/twitter_shared_button.es6.jsx
+++ app/assets/javascripts/components/shared/twitter_shared_button.es6.jsx
@@ -1,21 +1,47 @@
 class TwitterSharedButton extends BaseComponent {
+  constructor(props)  {
+    super()
+    this.state = {
+      text: props.text,
+      display: 'none'
+    }
+  }
+
   // 一度コンポーネントが描画されたら再描画は行わない
   shouldComponentUpdate(nextProps, nextState) { return false }
 
   componentDidMount() {
-    widgetoon_main()
+    if (typeof(twttr) === 'undefined') {
+      twitterjs = document.createElement('script')
+      twitterjs.async = true
+      twitterjs.src = '//platform.twitter.com/widgets.js'
+      document.getElementsByTagName('body')[0].appendChild(twitterjs)
+    } else {
+      twttr.widgets.load(document.getElementById('twitter-shared-button'))
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.display !== 'none') {
+      return null
+    }
+    this.setState({display: 'block'})
   }
 
   render() {
     return (
       <a
+        id='twitter-shared-button'
+        style={{display: this.state.display}}
         href='https://twitter.com/share'
-        className='twitter-share-buttoon'
-        data-url='https://iidx12.tk'
-        data-text={this.props.text}
-        data-count='horizontal'
+        data-text={this.state.text}
+        data-lang='ja'
         data-size='large'
-        data-lang='ja'>ツイート</a>
+        data-related='IIDX_12'
+        className='twitter-share-button'
+      >
+        ツイート
+      </a>
     )
   }
 }
