cf16f1d7c765d7d570153b793e739928315e7320
839 <8398a7@gmail.com>
2017-02-12 22:46:04 +0900

    [#599] add help page for official synchronization

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 class HelpsController < ApplicationController
+  def official
+    domain = Rails.env.development? ? request.host_with_port : request.host
+    @bookmarklet = %(javascript:(!function(){var d=document;var s=d.createElement("script");s.id="abilitysheet";s.type="text/javascript";s.src="#{request.scheme}://#{domain}/loader.js";d.head.appendChild(s);}());)
+  end
 end
