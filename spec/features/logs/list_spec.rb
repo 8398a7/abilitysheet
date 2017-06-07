7a57955db31ccf92ec32505af61b7c78e13e9cbd
839 <8398a7@gmail.com>
2017-06-07 22:11:58 +0900

    [#979] add feature spec through log list page

---
+++ spec/features/logs/list_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+feature 'ログ一覧' do
+  given(:user) { create(:user, id: 1) }
+  background do
+    login(user)
+    visit list_log_path(user.iidxid)
+  end
+  scenario 'ログ一覧ページが閲覧できる' do
+    expect(page).to have_content('更新履歴')
+  end
+end
