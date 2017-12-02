15e0ed11f3e675d283a44e09dc2aff919b82d0b8
839 <8398a7@gmail.com>
2017-12-03 06:12:07 +0900

    [#995] use headless chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,12 +16,24 @@ RSpec.configure do |config|
   end
 
   Capybara.raise_server_errors = false
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome)
+  end
 
   # rspec retry setting
   config.verbose_retry = true
   config.display_try_failure_messages = true
   config.around :each do |ex|
     ex.run_with_retry retry: ENV['RETRY_RSPEC']
+  end
+
+  config.before(:each, type: :system) do |example|
+    if example.metadata[:js]
+      caps = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: %w[--headless --disable-gpu] })
+      driven_by :selenium, screen_size: [1400, 1400], options: { desired_capabilities: caps }
+    else
+      driven_by :rack_test
+    end
   end
 
   Sidekiq::Testing.inline!
