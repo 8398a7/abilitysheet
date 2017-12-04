e5664be3b16eba2afb22986db1a49a629a6a66b5
839 <8398a7@gmail.com>
2017-12-05 08:12:36 +0900

    [#1005] prioritize new log data

--- app/services/clearing_transition_table_service.rb
+++ app/services/clearing_transition_table_service.rb
@@ -7,9 +7,10 @@ class ClearingTransitionTableService < ApplicationService
 
   def execute
     hash = {}
-    sheets = Sheet.active.joins(:logs).select('sheets.title, logs.*').where('logs.user_id = ? AND logs.new_state != logs.pre_state', @user_id)
+    sheets = Sheet.active.joins(:logs).select('sheets.title, logs.new_state, logs.created_date').where('logs.user_id = ? AND logs.new_state != logs.pre_state', @user_id)
     sheets.each do |sheet|
       hash[sheet.title] ||= {}
+      next if hash[sheet.title][sheet.new_state] && hash[sheet.title][sheet.new_state] > sheet.created_date
       hash[sheet.title][sheet.new_state] = sheet.created_date
     end
     hash
