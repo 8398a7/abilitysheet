e1d9ca828c1046926244bca878035689ab54e1d2
839 <8398a7@gmail.com>
2017-12-12 21:05:34 +0900

    [#1018] remove navbar components

--- app/assets/javascripts/components/navbar/conntact.es6.jsx
+++
@@ -1,9 +0,0 @@
-class Conntact extends BaseComponent {
-  render() {
-    return (
-      <li>
-        <a href={new_message_path()}><i className='fa fa-phone' />連絡フォーム</a>
-      </li>
-    )
-  }
-}
