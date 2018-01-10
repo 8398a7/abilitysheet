982cb743c2438df86f95efde1747b832c0d2f266
839 <8398a7@gmail.com>
2018-01-10 21:25:35 +0900

    control the number of retries

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,10 @@ redis_params = { url: ENV['REDIS_URL'] }
 
 Sidekiq.configure_server do |config|
   config.redis = redis_params
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Middleware::Server::RetryJobs, max_retries: 0
-  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = redis_params
 end
+
+Sidekiq.default_worker_options = { retry: 0 }
