31710ac829d2d7d56f5dbfd8c68659d12c20fb1b
839 <8398a7@gmail.com>
2018-11-23 16:33:44 +0900

    [#986] log can be seen with object

--- app/javascript/lib/store.ts
+++ app/javascript/lib/store.ts
@@ -5,7 +5,13 @@ import createSagaMiddleware, { SagaMiddleware } from 'redux-saga';
 export default function storeCreator<T>(props: T, rootReducer: Reducer<any>, rootSaga?: any) {
   const middlewares: Middleware[] = [];
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(createLogger());
+    middlewares.push(createLogger({
+      stateTransformer: (state) => {
+        const newState: any = {};
+        for (const i of Object.keys(state)) { newState[i] = state[i].toJS(); }
+        return newState;
+      },
+    }));
   }
   const devtools: any = process.env.NODE_ENV !== 'production' && (window as any).devToolsExtension ?
     (window as any).devToolsExtension() : (f: any) => f;
@@ -15,12 +21,9 @@ export default function storeCreator<T>(props: T, rootReducer: Reducer<any>, roo
   const store = createStore(
     rootReducer,
     compose(
-      applyMiddleware(
-        sagaMiddleware,
-        ...middlewares,
-      ),
+      applyMiddleware(sagaMiddleware, ...middlewares),
       devtools,
-    )
+    ),
   );
   if (rootSaga) { sagaMiddleware.run(rootSaga); }
 
