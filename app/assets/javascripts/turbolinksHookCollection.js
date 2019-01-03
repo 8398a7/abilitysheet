15a2af73dc1159b361cdbd5bba7547955eb666b8
839 <8398a7@gmail.com>
2019-01-03 13:17:12 +0900

    [#1169] remove return

--- app/assets/javascripts/turbolinksHookCollection.js
+++ app/assets/javascripts/turbolinksHookCollection.js
@@ -1,11 +1,11 @@
 document.addEventListener('turbolinks:request-start', function() {
   document.querySelector('.loading-container').style = 'display: block';
-  return NProgress.start();
+  NProgress.start();
 });
 
 document.addEventListener('turbolinks:render', function() {
   document.querySelector('.loading-container').style = 'display: none';
-  return NProgress.done();
+  NProgress.done();
 });
 
 document.addEventListener('turbolinks:load', function() { $('.searchable').select2() });
