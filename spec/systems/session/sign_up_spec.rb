d1f1baa432ad01cd41e937292450e1f16a7b1469
839 <8398a7@gmail.com>
2019-04-29 22:54:30 +0900

    [#1219] fix for rubocop

--- spec/systems/session/sign_up_spec.rb
+++ spec/systems/session/sign_up_spec.rb
@@ -20,21 +20,21 @@ feature 'sign up', type: :system do
 
   scenario 'ISTに存在しないユーザの場合でも登録できる' do
     expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_falsey
-    expect {
+    expect do
       VCR.use_cassette('not_found_ist') do
         input_sign_up_form('1234-5678')
       end
-    }.to change { User.count }.by(1)
+    end.to change { User.count }.by(1)
     expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_truthy
   end
   scenario 'ISTに存在するユーザなら同期して登録できる' do
     expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_falsey
     iidxid = '8594-9652'
-    expect {
+    expect do
       VCR.use_cassette('ist') do
         input_sign_up_form(iidxid)
       end
-    }.to change { User.count }.by(1)
+    end.to change { User.count }.by(1)
     user = User.find_by(iidxid: iidxid)
     expect(user.present?).to be_truthy
     expect(user.scores.is_current_version.find_by(sheet: Sheet.find_by!(title: 'AA')).score).to eq 3129
