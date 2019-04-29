cff1c5e133ced948ffd6fd41ac465194782c4d75
839 <8398a7@gmail.com>
2019-04-29 22:51:16 +0900

    close #1219 synchronize with IST at new registration

--- spec/systems/session/sign_up_spec.rb
+++ spec/systems/session/sign_up_spec.rb
@@ -5,20 +5,38 @@ feature 'sign up', type: :system do
     visit new_user_registration_path
     sync_sheet
     allow(Slack::UserDispatcher).to receive(:new_register_notify).and_return(true)
-    allow(ManagerJob).to receive(:perform_later).and_return(true)
   end
-  scenario '新規登録を行う' do
-    expect(User.count).to eq 0
+  def input_sign_up_form(iidxid)
     fill_in 'user_email', with: 'sign_up_spec@mail.iidx12.tk'
     fill_in 'user_username', with: 'signup'
     fill_in 'user_djname', with: 'SIGNUP'
-    fill_in 'user_iidxid', with: '4362-8324'
+    fill_in 'user_iidxid', with: iidxid
     fill_in 'user_password', with: 'hogehoge'
     fill_in 'user_password_confirmation', with: 'hogehoge'
     select '京都府', from: 'user_pref'
     select '八段', from: 'user_grade'
     click_button '登録'
-    expect(User.count).to eq 1
-    expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to eq true
+  end
+
+  scenario 'ISTに存在しないユーザの場合でも登録できる' do
+    expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_falsey
+    expect {
+      VCR.use_cassette('not_found_ist') do
+        input_sign_up_form('1234-5678')
+      end
+    }.to change { User.count }.by(1)
+    expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_truthy
+  end
+  scenario 'ISTに存在するユーザなら同期して登録できる' do
+    expect(User.exists?(email: 'sign_up_spec@mail.iidx12.tk')).to be_falsey
+    iidxid = '8594-9652'
+    expect {
+      VCR.use_cassette('ist') do
+        input_sign_up_form(iidxid)
+      end
+    }.to change { User.count }.by(1)
+    user = User.find_by(iidxid: iidxid)
+    expect(user.present?).to be_truthy
+    expect(user.scores.is_current_version.find_by(sheet: Sheet.find_by!(title: 'AA')).score).to eq 3129
   end
 end
