d8b171092ab4cb402c07fe32abab96da849eab36
839 <8398a7@gmail.com>
2019-07-07 18:38:45 +0900

    [#1255] add sentry job

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -4,3 +4,5 @@ const environment = require('./environment');
 
 module.exports = environment.toWebpackConfig();
 module.exports.devtool = 'none';
+module.exports.devtool = 'source-map'
+process.env.RELEASE = 'v0.0.1-15-gac7d2eeb9e'
--- scripts/enable-source-map-precompile.sh
+++ scripts/enable-source-map-precompile.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -xe
 
 echo "module.exports.devtool = 'source-map'" >> config/webpack/production.js
-echo "process.env.RELEASE = '$(git describe --tags --abbrev=10)'" >> config/webpack/production.js
+echo "process.env.RELEASE = 'abilitysheet@$(git describe --tags --abbrev=10)'" >> config/webpack/production.js
 RAILS_ENV=production rails assets:precompile
+
+SENTRY_PROJECT=abilitysheet-frontend
+tag=$(git describe --tags --abbrev=10)
+sentry-cli releases new -p ${SENTRY_PROJECT} abilitysheet@${tag}
+sentry-cli releases files abilitysheet@${tag} upload-sourcemaps ./public/packs/js --rewrite
+sentry-cli releases finalize abilitysheet@${tag}
