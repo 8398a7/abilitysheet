7f6c87609eafff934f82aab7d859ea9615969118
839 <8398a7@gmail.com>
2019-07-07 17:53:47 +0900

    [#1255] remove source map

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,6 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-const environment = require('./environment')
+const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig()
-module.exports.devtool = 'source-map'
+module.exports = environment.toWebpackConfig();
+module.exports.devtool = 'none';
