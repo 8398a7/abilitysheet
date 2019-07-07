b7b96bf7fa8830dcb4da80309ffaab159e5f9e4d
839 <8398a7@gmail.com>
2019-07-07 18:55:27 +0900

    [#1255] enable sentry job only master branch

--- scripts/enable-source-map-precompile.sh
+++ scripts/enable-source-map-precompile.sh
@@ -4,8 +4,7 @@ echo "module.exports.devtool = 'source-map'" >> config/webpack/production.js
 echo "process.env.RELEASE = 'abilitysheet@$(git describe --tags --abbrev=10)'" >> config/webpack/production.js
 RAILS_ENV=production bundle exec rails ts:routes assets:precompile
 
-SENTRY_PROJECT=abilitysheet-frontend
 tag=$(git describe --tags --abbrev=10)
 sentry-cli releases new -p ${SENTRY_PROJECT} abilitysheet@${tag}
 sentry-cli releases files abilitysheet@${tag} upload-sourcemaps ./public/packs/js --rewrite
-SENTRY_PROJECT=abilitysheet-frontend sentry-cli releases finalize abilitysheet@${tag}
+sentry-cli releases finalize abilitysheet@${tag}
