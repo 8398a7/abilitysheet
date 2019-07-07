dc8e03811fdfb3b8fbdfbeef300bd8a157858026
839 <8398a7@gmail.com>
2019-07-07 18:28:37 +0900

    [#1255] set env and release

--- app/javascript/lib/store.ts
+++ app/javascript/lib/store.ts
@@ -1,36 +1,64 @@
 import createRavenMiddleware from 'raven-for-redux';
 import Raven from 'raven-js';
-import { applyMiddleware, compose, createStore, Middleware, Reducer } from 'redux';
+import {
+  applyMiddleware,
+  compose,
+  createStore,
+  Middleware,
+  Reducer,
+} from 'redux';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware, { SagaMiddleware } from 'redux-saga';
 import { ForkEffect } from 'redux-saga/effects';
 
-export default function storeCreator<S>(props: AbilitysheetContext, rootReducer: Reducer<any>, rootSaga: () => IterableIterator<ForkEffect>, initialState: S) {
+export default function storeCreator<S>(
+  props: AbilitysheetContext,
+  rootReducer: Reducer<any>,
+  rootSaga: () => IterableIterator<ForkEffect>,
+  initialState: S,
+) {
   const middlewares: Middleware[] = [];
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(createLogger({
-      stateTransformer: (state) => {
-        const newState: any = {};
-        for (const i of Object.keys(state)) { newState[i] = state[i].toJS(); }
-        return newState;
-      },
-    }));
+    middlewares.push(
+      createLogger({
+        stateTransformer: state => {
+          const newState: any = {};
+          for (const i of Object.keys(state)) {
+            newState[i] = state[i].toJS();
+          }
+          return newState;
+        },
+      }),
+    );
   }
-  const devtools: any = process.env.NODE_ENV !== 'production' && (window as any).__REDUX_DEVTOOLS_EXTENSION__ ?
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__() : (f: any) => f;
+  const devtools: any =
+    process.env.NODE_ENV !== 'production' &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__
+      ? (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+      : (f: any) => f;
 
   const sagaMiddleware: SagaMiddleware<{}> = createSagaMiddleware();
 
-  Raven.config(props.context.sentry_dsn).install();
+  const options: Raven.RavenOptions = {
+    environment: process.env.NODE_ENV,
+    release: process.env.RELEASE,
+  };
+  Raven.config(props.context.sentry_dsn, options).install();
   const store = createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(sagaMiddleware, createRavenMiddleware(Raven, {}), ...middlewares),
+      applyMiddleware(
+        sagaMiddleware,
+        createRavenMiddleware(Raven, {}),
+        ...middlewares,
+      ),
       devtools,
     ),
   );
-  if (rootSaga) { sagaMiddleware.run(rootSaga); }
+  if (rootSaga) {
+    sagaMiddleware.run(rootSaga);
+  }
 
   return store;
 }
--- scripts/enable-source-map-precompile.sh
+++ scripts/enable-source-map-precompile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
 echo "module.exports.devtool = 'source-map'" >> config/webpack/production.js
+echo "process.env.RELEASE = '$(git describe --tags --abbrev=10)'" >> config/webpack/production.js
 RAILS_ENV=production rails assets:precompile
