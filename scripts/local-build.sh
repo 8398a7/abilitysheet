2abac172789d7b613b3ba870318a7db667abf878
839 <8398a7@gmail.com>
2019-07-07 17:48:16 +0900

    use environment variable for project

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-docker build --cache-from gcr.io/iidx-app/abilitysheet-base:latest -t gcr.io/iidx-app/abilitysheet-base:latest . -f build/base.Dockerfile
-docker build --cache-from gcr.io/iidx-app/abilitysheet:latest -t gcr.io/iidx-app/abilitysheet:latest . -f build/Dockerfile
-docker push gcr.io/iidx-app/abilitysheet-base:latest
-docker push gcr.io/iidx-app/abilitysheet:latest
+docker build --cache-from gcr.io/${GCLOUD_PROJECT}/abilitysheet-base:latest -t gcr.io/${GCLOUD_PROJECT}/abilitysheet-base:latest . -f build/base.Dockerfile
+docker build --cache-from gcr.io/${GCLOUD_PROJECT}/abilitysheet:latest -t gcr.io/${GCLOUD_PROJECT}/abilitysheet:latest . -f build/Dockerfile
+docker push gcr.io/${GCLOUD_PROJECT}/abilitysheet-base:latest
+docker push gcr.io/${GCLOUD_PROJECT}/abilitysheet:latest
