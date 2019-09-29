398ed9bec653b69627dda6ca7511a2dca4e31929
839 <8398a7@gmail.com>
2019-09-29 14:17:52 +0900

    fix local build script

--- scripts/local-build.sh
+++ scripts/local-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
+base=docker.pkg.github.com/8398a7/abilitysheet/abilitysheet-base:latest
 tag=$(git describe --tags --abbrev=10)
 
-docker build --cache-from 8398a7/abilitysheet-base:latest -t 8398a7/abilitysheet-base:latest . -f build/base.Dockerfile
-docker push 8398a7/abilitysheet-base:latest
+docker build --cache-from $base -t $base -f build/base.Dockerfile .
+docker push $base
 docker build -t gcr.io/${GCLOUD_PROJECT}/abilitysheet:$tag . -f build/Dockerfile
 docker push gcr.io/${GCLOUD_PROJECT}/abilitysheet:$tag
