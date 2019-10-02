6fe7279a8c84990eed47b508d68b08f74a2d55a2
839 <8398a7@gmail.com>
2019-10-02 19:31:06 +0900

    [#1250] remove connect from Welcome Component

--- app/javascript/app/Welcome/components/index.tsx
+++ app/javascript/app/Welcome/components/index.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import SplineGraph from '../../../lib/components/SplineGraph';
 import { RootState } from '../../../lib/ducks';
 import Adsenses from '../../Adsense/Adsenses';
 import TopPanel from './TopPanel';
 import TwitterContents from './TwitterContents';
 
-function mapStateToProps(state: RootState) {
-  return {
-    $$currentUser: state.$$meta.currentUser,
-    mobile: state.$$meta.env.mobileView(),
-  };
-}
-type Props = ReturnType<typeof mapStateToProps>;
-const Welcome: React.SFC<Props> = ({ $$currentUser: user, mobile }) => (
-  <div>
-    <TopPanel {...{ user, mobile }} />
-    <Adsenses slot={1} />
-    <div>
-      SP ☆12以外を管理したい人向け: <a href="https://score.iidx.app" target="_blank">IIDX Score Table</a>
-      <br />
-      SP/DP両対応のスコア/ランプ管理アプリです。
-    </div>
-    <TwitterContents />
-    <br />
-    <div className="uk-panel uk-panel-box">
-      <h3 className="uk-panel-title">可視化例</h3>
-      <div className="center">
-        <SplineGraph initialRender={true} />
+const Welcome: React.SFC = () => {
+  const user = useSelector((state: RootState) => state.$$meta.currentUser);
+  const mobile = useSelector((state: RootState) =>
+    state.$$meta.env.mobileView(),
+  );
+  return (
+    <>
+      <TopPanel {...{ user, mobile }} />
+      <Adsenses slot={1} />
+      <div>
+        SP ☆12以外を管理したい人向け:{' '}
+        <a href="https://score.iidx.app" target="_blank">
+          IIDX Score Table
+        </a>
+        <br />
+        SP/DP両対応のスコア/ランプ管理アプリです。
       </div>
-    </div>
-  </div>
-);
 
-export default connect(mapStateToProps)(Welcome);
+      <TwitterContents />
+      <br />
+      <div className="uk-panel uk-panel-box">
+        <h3 className="uk-panel-title">可視化例</h3>
+        <div className="center">
+          <SplineGraph initialRender={true} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Welcome;
