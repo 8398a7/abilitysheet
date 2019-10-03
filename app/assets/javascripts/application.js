dfc00cf17bf345bab35080dd12eb6da867f7cbfb
839 <8398a7@gmail.com>
2019-10-03 22:00:22 +0900

    [#1144] remove uikit and add bulma

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,9 +8,6 @@
 //= require peek/views/rblineprof
 //= require datatables-all/media/js/jquery.dataTables.min
 //= require datatables-all/media/js/dataTables.uikit.min
-//= require uikit/dist/js/uikit.min
-//= require uikit/dist/js/components/notify.min
-//= require uikit/dist/js/components/accordion.min
 //= require select2/dist/js/select2.min
 //= require nprogress/nprogress
 //= require turbolinks
