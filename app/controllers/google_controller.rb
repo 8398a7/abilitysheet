845803330fddfc2c4596dd5279df1ca86363eb03
839 <8398a7@gmail.com>
2019-11-03 18:19:22 +0900

    [#1364] fix for rubocop

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class GoogleController < ApplicationController
   before_action :authenticate_user!
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   before_action :authenticate_user!
 
