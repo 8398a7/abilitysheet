f8bee9def0e70a5752bbb56258902e6ba585e79f
839 <8398a7@gmail.com>
2019-11-03 17:47:05 +0900

    [#1364] [command] rails db:migrate

---
+++ spec/models/social_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: socials
+#
+#  id         :bigint(8)        not null, primary key
+#  provider   :string
+#  raw        :json
+#  secret     :string
+#  token      :string
+#  uid        :string
+#  user_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_socials_on_user_id_and_provider  (user_id,provider) UNIQUE
+#
+
+describe Social, type: :model do
+end
