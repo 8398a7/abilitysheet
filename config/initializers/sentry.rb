0908d5d2b014ea0350a05abb20315847859d0e12
839 <8398a7@gmail.com>
2022-10-23 17:18:55 +0900

    [#1481] update sentry setting

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
-
-logger = ::Logger.new('log/raven.log')
-logger.level = ::Logger::DEBUG
-
-Raven.configure do |config|
-  config.logger = logger
+Sentry.init do |config|
+  config.breadcrumbs_logger = [:monotonic_active_support_logger, :http_logger, :sentry_logger]
+  config.background_worker_threads = 0
+  config.send_default_pii = true
+  config.traces_sample_rate = 0.0 # set a float between 0.0 and 1.0 to enable performance monitoring
   config.dsn = ENV['SENTRY_DSN']
-  config.environments = %w(staging production)
-  config.current_environment = Rails.env
-  config.release = ENV.fetch('RELEASE', 'vx.y.z')
+  config.capture_exception_frame_locals = true
+  config.enabled_environments = %w[development production]
 end
